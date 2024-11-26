b9c68b60a2d0fc3edaaf592df5ea0cc5ed706f5e
eliudjc <ecampos@coasa.com.ar>
2024-11-25 21:37:16 -0300

    canonical

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,19 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import { ThemeProvider } from '@/components/theme-provider';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import BotLayout from '@/components/BotLayout';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import BotLayout from "@/components/BotLayout";
 
-const inter = Inter({ subsets: ['latin'] });
-
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Digital Marketing Excellence',
-  description: 'Boost your business with our digital marketing solutions',
-  // Agrega la metaetiqueta aquí
-  metadataBase: new URL('https://www.macawminds.com.ar/'), // Cambia a tu dominio
+  title: "Digital Marketing Excellence",
+  description: "Boost your business with our digital marketing solutions",
+  metadataBase: new URL("https://www.macawminds.com.ar/"), 
   verification: {
-    google: 'tj8qy9wsSoAWTcHjvNqkZ-3vT4FT2JBrW3eRixpDlSQ', // Clave de verificación
+    google: "tj8qy9wsSoAWTcHjvNqkZ-3vT4FT2JBrW3eRixpDlSQ", 
   },
 };
 
@@ -26,9 +24,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-         <head>
+      <head>
         {/* Agrega la referencia al favicon */}
         <link rel="icon" type="image/png" href="/favicon.svg" />
+        <link rel="canonical" href="https://www.macawminds.com.ar/" />
       </head>
       <body className={inter.className}>
         <BotLayout>
